7a15053155b5d5476357272c44fd29c58d0cfcbc
Marink211 <marink211@gmail.com>
2020-11-17 02:34:39 +0200

    first

---
+++ basicMath.c
@@ -0,0 +1,19 @@
+nclude "myMath.h"
+
+float add (float x , float y) {
+
+	            return x+y;
+}
+
+float sub(float x , float y) {
+
+	            return x-y;
+}
+double mul(double x , int y){
+
+	            return x*y;
+}
+double div(double x, int y) {
+
+	            return x/y;
+}
