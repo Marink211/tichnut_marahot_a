27c6bacbfe40df036889380abad65a8faafabf1e
Marink211 <marink211@gmail.com>
2020-11-17 02:35:25 +0200

    first

---
+++ myMath.h
@@ -0,0 +1,18 @@
+fndef MYMATH
+#define BASICMATH
+#define POWER
+
+
+// x+y
+float add(float x,float y);
+// x-y
+float sub(float x,float y);
+//y*x
+double mul(double x,int y);
+//x/y
+double div(double x,int y);
+//e^x
+double Exponent(int x);
+//x^y
+double Power(double x,int y);
+#endif
